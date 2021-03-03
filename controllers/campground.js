490055a70f2c8239e14c7fd9af5c8e1e16fe0c60
Conor <cjhfitz@gmail.com>
2021-03-02 19:17:42 -0500

    add error handling

--- controllers/campground.js
+++ controllers/campground.js
@@ -1,36 +1,39 @@
 const Campground = require('../models/campground');
 
 
-module.exports.index = async (req, res) => {
-    try {
-        throw new Error('oops');
-        const campgrounds = await Campground.find({});
-        res.send(campgrounds);
-    } catch(e) {
-        res.send(e.message)
+module.exports.index = wrapAsync(async (req, res) => {
+    const campgrounds = await Campground.find({});
+    res.send(campgrounds); 
+});
+
+// returns a new function
+// executes function passed and adds .catch() and handles error
+function wrapAsync(fn) {
+    return function(req, res, next) {
+        fn(req, res, next).catch(err => next(err));
     }
-}
+};
 
 // For SHOW and EDIT routes
-module.exports.getCampground = async (req, res) => {
+module.exports.getCampground = wrapAsync(async (req, res) => {
     const campground = await Campground.findById(req.params.id);
     res.send(campground);
-}
+});
 
-module.exports.createCampground = async (req, res) => {
+module.exports.createCampground = wrapAsync(async (req, res) => {
     const newCampground = new Campground(req.body.campground);
     await newCampground.save();
-}
+});
 
 // module.exports.editCampground = async (req, res) => {
 //     const campground = await Campground.findById(req.params.id);
 //     res.send(campground);
 // }
 
-module.exports.updateCampground = async (req, res) => {
+module.exports.updateCampground = wrapAsync(async (req, res) => {
     await Campground.findByIdAndUpdate(req.params.id, req.body);
-}
+});
 
-module.exports.deleteCampground = async (req, res) => {
+module.exports.deleteCampground = wrapAsync(async (req, res) => {
     await Campground.findByIdAndDelete(req.params.id);
-}
+});
